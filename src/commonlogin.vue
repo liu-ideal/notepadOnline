a9678f8587a31dbdeadc0d466e9309df85e28b04
liu-ideal <liuideal@sina.com>
2020-01-06 08:00:42 +0800

    l

---
+++ src/commonlogin.vue
@@ -0,0 +1,33 @@
+<template lang="html">
+<div class="wrap">
+  <div class="wrap_inner">
+    <h4>注册</h4>
+    <div class="username">
+      <p>Username:</p>
+      <input type="text" name="" value="" placeholder="输入登录帐号">
+    </div>
+    <div class="password">
+      <p>Password:</p>
+      <input type="text" name="" value="" placeholder="输入登录密码">
+    </div>
+    <div class="remenber_submit">
+        <div class="">
+          <label><input type="checkbox" name="" value="">记住帐号和密码</label>
+        </div>
+        <div class="submit">
+          <button type="button" name="button">提交</button>
+        </div>
+    </div>
+  </div>
+
+</div>
+</template>
+
+<script>
+export default {
+  name:"commonlogin"
+}
+</script>
+
+<style lang="scss" scoped>
+</style>
